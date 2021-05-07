c4897f4bf5d70e5dac33c246effddb52e0ec5f5d
Lucas Yunkyu Lee <me@luc.li>
2021-05-07 23:53:22 +0900

    feat: basic single file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { FilesModule } from "./files/files.module"
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
